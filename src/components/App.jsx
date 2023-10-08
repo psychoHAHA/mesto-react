624820d5f67f45622635575c9ea83123c063c17d
kirill_zernovv <80909413+psychoHAHA@users.noreply.github.com>
2023-10-09 00:07:17 +0500

    вторая итерация

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -78,6 +78,7 @@ function App() {
       setCurrentUser(res)
       closeAllPopups()
     })
+    .catch((err) => alert(err))
   }
 
   function handleUpdateAvatar(value) {
@@ -86,6 +87,7 @@ function App() {
       setCurrentUser(res)
       closeAllPopups()
     })
+    .catch((err) => alert(err))
   }
 
   function handleAddPlaceSubmit(value) {
@@ -94,6 +96,7 @@ function App() {
       setCards([newCard, ...cards])
       closeAllPopups()
     })
+    .catch((err) => alert(err))
   }
 
   function handleCardDelete(card) {
@@ -102,6 +105,7 @@ function App() {
     .then(() => {
       setCards((state) => state.filter((c) => c._id !== card._id))
     })
+    .catch((err) => alert(err))
   }
 
   return (
--- src/components/EditAvatarPopup.jsx
+++ src/components/EditAvatarPopup.jsx
@@ -1,48 +1,42 @@
-import React, { useEffect, useState } from "react"
-import { CurrentUserContext } from "../contexts/CurrentUserContext"
-import PopupWithForm from "./PopupWithForm"
+import React, { useEffect, useState } from "react";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
+import PopupWithForm from "./PopupWithForm";
 
-function EditAvatarPopup({ 
-  isOpen,
-  onClose,
-  onCloseOverlay,
-  onUpdateAvatar
- }) {
-
-  const currentUser = React.useContext(CurrentUserContext)
-  const linkAvatar = React.useRef()
+function EditAvatarPopup({ isOpen, onClose, onCloseOverlay, onUpdateAvatar }) {
+  const currentUser = React.useContext(CurrentUserContext);
+  const linkAvatar = React.useRef();
 
   useEffect(() => {
-    linkAvatar.current.value = ''
-  }, [currentUser])
+    linkAvatar.current.value = "";
+  }, [currentUser]);
 
   function handleSubmit(e) {
-    e.preventDefault()
-    onUpdateAvatar({avatar: linkAvatar.current.value})
+    e.preventDefault();
+    onUpdateAvatar({ avatar: linkAvatar.current.value });
   }
-  
+
   return (
-    <PopupWithForm 
+    <PopupWithForm
       isOpen={isOpen}
       onClose={onClose}
       onCloseOverlay={onCloseOverlay}
       onSubmit={handleSubmit}
-      title='Обновить профиль'
-      buttonText='Сохранить'
-      name='avatar'
-      >
-      <input
-      type="url"
-      placeholder="Ссылка на аватар профиля"
-      className="popup__input popup-avatar__input popup-avatar__input_edit_image-url"
+      title="Обновить профиль"
+      buttonText="Сохранить"
       name="avatar"
-      required
-      id="avatar-input"
-      ref={linkAvatar}
+    >
+      <input
+        type="url"
+        placeholder="Ссылка на аватар профиля"
+        className="popup__input popup-avatar__input popup-avatar__input_edit_image-url"
+        name="avatar"
+        required
+        id="avatar-input"
+        ref={linkAvatar}
       />
       <span className="popup__input-error avatar-input-error"></span>
     </PopupWithForm>
-  )
+  );
 }
 
-export default EditAvatarPopup
+export default EditAvatarPopup;
